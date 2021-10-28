c47363e81069c06eb560291bc2755b072575ae91
parascseb <91192621+parascseb@users.noreply.github.com>
2021-10-28 09:54:17 +0530

    Add files via upload

---
+++ Hartal Problem.py
@@ -0,0 +1,22 @@
+cases = int(input())
+
+for c in range(cases):
+  n = int(input())
+
+  p = int(input())
+  periods = []
+  for i in range(p):
+    periods.append(int(input()))
+
+  is_hartal = [False]*(n+1)
+
+  for p in periods:
+    cp = p
+    while cp <= n:
+      if not is_hartal[cp]:
+        mod = cp % 7
+        if mod != 6 and mod != 0:
+          is_hartal[cp] = True
+      cp += p
+
+  print(sum(is_hartal)) 
